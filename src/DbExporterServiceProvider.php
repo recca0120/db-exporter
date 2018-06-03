52b7028dd7e6fbf026fa4ef965f0c1d05fe98683
recca0120 <recca0120@gamil.com>
2018-06-03 14:54:00 +0800

    service provider

---
+++ src/DbExporterServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Recca0120\DbExporter;
+
+use Illuminate\Support\ServiceProvider;
+use Recca0120\DbExporter\Dsn\Factory as DsnFactory;
+
+class DbExporterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/db-exporter.php', 'db-exporter');
+
+        $this->app->singleton(DbExporterManager::class, function ($app) {
+            return new DbExporterManager($app, new DumperFactory(new DsnFactory()));
+        });
+    }
+}
