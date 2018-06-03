406fc66e1476235fb5a797d98cb73289d6d4add3
recca0120 <recca0120@gamil.com>
2018-06-03 10:19:54 +0800

    create driver

--- src/DbExporterManager.php
+++ src/DbExporterManager.php
@@ -7,13 +7,33 @@ use Illuminate\Support\Manager;
 
 class DbExporterManager extends Manager
 {
-    public function __construct($app)
+    private $factory;
+
+    public function __construct($app, DumperFactory $factory = null)
     {
         parent::__construct($app);
+        $this->factory = $factory ?: new DumperFactory;
     }
 
     public function getDefaultDriver()
     {
-        return Arr::get($this->app['config'], 'database.default', 'mysql');
+        return Arr::get($this->app['config'], 'database.default');
+    }
+
+    protected function createDriver($driver)
+    {
+        $connection = $this->getConnection($driver);
+
+        return new DbExporter(
+            $this->factory->create(
+                $connection,
+                Arr::get($this->app['config'], 'db-exporter', [])
+            )
+        );
+    }
+
+    private function getConnection($driver)
+    {
+        return Arr::get($this->app['config'], 'database.connections.'.$driver);
     }
 }
